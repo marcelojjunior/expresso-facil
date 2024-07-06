dd2f22a5450083966b36555933fadfecb9d852a8
marcelojjunior <dbvmarcelo13@gmail.com>
2024-07-05 22:03:31 -0300

    fix: test route deploy

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -11,7 +11,7 @@ import DefaultLayout from "../layouts/DefaultLayout";
 export const routes = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<Outlet />}>
-            <Route path="/" element={<DefaultLayout />}>
+            <Route path="/expresso-facil" element={<DefaultLayout />}>
                 <Route index element={<Home />} />
                 <Route path="/tracking" element={<Tracking />} />
                 <Route path="/contact" element={<Contact />} />
